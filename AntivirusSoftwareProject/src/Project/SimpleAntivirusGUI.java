30b7e4a6605f18ab0596ba7c127782929d084171
cookie4566 <kennedyc416@gmail.com>
2024-04-13 08:58:47 -0400

    Replaced the progress bar with a progress GUI (LMK if the gui doesn't work!)

--- AntivirusSoftwareProject/src/Project/SimpleAntivirusGUI.java
+++ AntivirusSoftwareProject/src/Project/SimpleAntivirusGUI.java
@@ -5,6 +5,7 @@ import javax.swing.text.BadLocationException;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
@@ -14,11 +15,40 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.beans.*;
+import java.util.Random;
 
-public class SimpleAntivirusGUI extends JFrame {
+public class SimpleAntivirusGUI extends JFrame impletments ActionListener,PropertyChangeListener{
+
     private JPanel resultPanel;
     private JTextField directoryTextField;
-
+    private ProgressMonitor progressMonitor;
+    private Task task;
+    
+    class Task extends SwingWorker<Void,Void>{
+    	@Override
+    	public Void doInBackground() {
+    		Random random = new Random();
+    		int progress = 0;
+    		setProgress(0);
+    		try {
+    			Thread.sleep(1000);
+    			while(progress < 100 && !isCancelled()) {
+    				Thread.sleep(random.nextInt(1000));
+    				progress += random.nextInt(10);
+    				setProgress(Math.min(progress,100));
+    			}
+    		} catch (InteruptedException ignore) {}
+    		return null;
+    	}
+    	@Override
+    	public void done() {
+    		Toolkit.getDefaultToolkit().beep();
+    		startButton.setEnabled(true);
+    		progressMonitor.setProgress(0);
+    	}
+    }
+    
     private List<String> virusSignatures;
     private static Map<String, String> maliciousFiles = new HashMap<>();
 
@@ -26,6 +56,7 @@ public class SimpleAntivirusGUI extends JFrame {
         setTitle("Simple Antivirus");
         setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     
 
         resultPanel = new JPanel();
         resultPanel.setLayout(new BoxLayout(resultPanel, BoxLayout.Y_AXIS));
@@ -36,6 +67,8 @@ public class SimpleAntivirusGUI extends JFrame {
         directoryTextField.setPreferredSize(new Dimension(200, 25)); // Set preferred size
         JButton selectDirectoryButton = new JButton("Select Directory");
         JButton scanButton = new JButton("Scan Directory");
+        
+
 
         JPanel inputPanel = new JPanel();
         inputPanel.setLayout(new FlowLayout());
@@ -44,9 +77,6 @@ public class SimpleAntivirusGUI extends JFrame {
         inputPanel.add(selectDirectoryButton);
         inputPanel.add(scanButton);
         
-        progressBar = new JProgressBar(0, task.getLengthOfTask());
-        progressBar.setValue(0);
-        progessBar.setStringPainted(true);
 
         getContentPane().add(inputPanel, BorderLayout.SOUTH);
 
@@ -80,19 +110,46 @@ public class SimpleAntivirusGUI extends JFrame {
                 selectDirectory();
             }
         });
-
+        // Added task method for scanner
         scanButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent evt) {
                 String directoryPath = directoryTextField.getText();
                 try {
                     scanDirectory(directoryPath);
+                    progressMonitor = new ProgressMonitor(ProgressMonitorGui.this,"Scanning :)", 0,100);
+                    progressMonitor.setProgress(0);
+                    task = new Task();
+                    task.addPropertyChangeListener(this)
+                    task.execute();
+                    scanButton.setEnabled(false);
                 } catch (IOException | NoSuchAlgorithmException | BadLocationException ex) {
                     ex.printStackTrace();
                 }
             }
-        });
-
+        })
+        
+        // method for progress bar
+        public void propertyChange(PropertyChangeEvent evt) {
+        	if ("progress" == evt.getPropertyName()) {
+        		int progress = (Integer) evt.getNewValue();
+        		progressMonitor.setProgress(progress);
+        		String mgs = String.format("Progress %d%%.\n", progress);
+        		progressMonitor.setNote(mgs);
+        		taskOutput.append(mgs);
+        		if(progressMonitor.isCanceled() || task.isDone()) {
+        			Toolkit.getDefaultToolkit().beep();
+        			if(progessMonitor.isCanceled()) {
+        				task.cancel(true);
+        				taskOutput.append("Scanning done.\n");
+        			}else {
+        				taskOutput.append("Scan done.\n");
+        			}
+        			scanButton.setEnabled(true);
+        		}
+        	}
+        }
+        }
         // Load virus signatures from file
         loadVirusSignatures();
 
@@ -100,6 +157,13 @@ public class SimpleAntivirusGUI extends JFrame {
         maliciousFiles.put("malicious_file.exe", "HASHVALUE1");
         maliciousFiles.put("another_malicious_file.dll", "HASHVALUE2");
     }
+// Monitor gui
+	public ProgressMonitorGui() {
+		super(new Borderlayout());
+		taskOutput = new JTextArea(5,30);
+		taskOutput.setMargin(new Insets(5,5,5,5));
+		taskOutput.setEditable(false);
+	}
 
     private void selectDirectory() {
         JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
@@ -165,6 +229,15 @@ public class SimpleAntivirusGUI extends JFrame {
 
         resultPanel.revalidate();
         resultPanel.repaint();
+    }
+    
+    private void done() {
+    	done = true;
+    	Toolkit.getDefaultToolkit().beep();
+    	startButton.setEnabled(true);
+    	setCursor(null):
+    	progressBar.setValue(progressBar.getMinimum());
+    	taskOutput.append("Done!!\n")
     }
 
     private boolean isMalicious(File file) throws NoSuchAlgorithmException, IOException {
